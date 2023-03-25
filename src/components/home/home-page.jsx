c78d38ba4ae4308122a165bb11db69d603912c54
Olena Nesteruk <pavlena81@gmail.com>
2023-03-25 23:55:03 +0200

    adds correctToHomePage

--- src/components/home/home-page.jsx
+++ src/components/home/home-page.jsx
@@ -5,17 +5,21 @@ import styles from '../../../styles/Home.module.css';
 export const HomePage = ({data}) => {
     return (
        <main className={styles.main}>
-        <div className={styles.description}>
-                  
-        </div>
-        {data.map((ev) => (
+        <div className={styles.home_body}>
+           {data.map((ev) => (
           <Link key={ev.id} href={`/events/${ev.id}`} passHref legacyBehavior>
-          <a>
-                    <Image className={styles.image} alt={ev.title} src={ev.image} width={200} height={250} />
-          <h2 className={styles.h2}>{ev.title}</h2>
-            <p className={styles.text_descript }>{ev.description}</p>
+            <a className={styles.card}>
+            <div className={styles.image}>
+            <Image  alt={ev.title} src={ev.image} width={600} height={400} />
+            </div>
+            <div className={styles.content}>
+            <h2>{ev.title}</h2>
+            <p>{ev.description}</p>
+            </div>
           </a></Link>
-        ))}         
+        ))}           
+        </div>
+             
       </main> 
     )
 }
