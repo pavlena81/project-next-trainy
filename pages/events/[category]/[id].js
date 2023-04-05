0ea4e1836b8c8cc61f41f06b80ce53c125cb5743
Olena Nesteruk <pavlena81@gmail.com>
2023-04-05 23:29:04 +0300

    add formToEvtPage

--- pages/events/[category]/[id].js
+++ pages/events/[category]/[id].js
@@ -1,11 +1,21 @@
 import Image from 'next/image'; 
+import styles from '../../../styles/Home.module.css';
 
 const EventPage = ({ data }) => {
     return (
         <div>            
-            <Image src={data.image} width={1000} height={500} alt={data.title} />
+            <Image src={data.image} width={900} height={500} alt={data.title} />
             <h1>{data.title}</h1>
             <p>{data.description}</p>
+            <form className={styles.email_registration}>
+               <input className={styles.input}
+                type="email"
+                id="email"
+                placeholder="Please insert your email here"/>
+                <button
+                    type="Submit"
+                className={styles.btn_registr}>Submit</button>
+            </form>
         </div>
     )
 }
