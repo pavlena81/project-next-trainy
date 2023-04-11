2d9243ef539ab384a18972e6f9caf787c97eb3ea
Olena Nesteruk <pavlena81@gmail.com>
2023-04-11 23:38:55 +0300

    adds firstPartApiRouting

--- src/components/events/eventCard.jsx
+++ src/components/events/eventCard.jsx
@@ -1,19 +1,36 @@
-import Image from 'next/image'; 
+import Image from 'next/image';
+import React, { useRef } from 'react'; 
+import { useRouter } from 'next/router';
 import styles from '../../../styles/Home.module.css';
 
-const SingleEvent = ({data}) => {
+const SingleEvent = ({ data }) => {
+    const inputEmail = useRef();
+    console.log(inputEmail);
+
+    const router = useRouter();
+    console.log(router);
+    const onSubmit = (e) => {
+        e.preventDefault();
+
+        const emailValue = inputEmail.current.value;
+        
+     };
+
      return (
-         <div> 
+         <div className="event_single_page"> 
             <h1>{data.title}</h1> 
             <Image src={data.image} width={900} height={500} alt={data.title} />
             <p>{data.description}</p>
-            <form className={styles.email_registration}>
+             <form className={styles.email_registration}
+             onSubmit={onSubmit}>
+                <label className={styles.label}>Get registered for this event!</label>
                <input className={styles.input}
+                ref={inputEmail}
                 type="email"
                 id="email"
                 placeholder="Please insert your email here"/>
                 <button
-                    type="Submit"
+                    type="button"
                 className={styles.btn_registr}>Submit</button>
             </form>
         </div>
