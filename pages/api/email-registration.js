55116bb2e84cde42841fadddbc49c63a0f46d7dc
Olena Nesteruk <pavlena81@gmail.com>
2023-04-18 00:49:39 +0300

    adds changesToExtractData

--- pages/api/email-registration.js
+++ pages/api/email-registration.js
@@ -5,28 +5,62 @@ function buildPath() {
   return path.join(process.cwd(), 'data', 'data.json');
 }
 
-const { method } = req;
-if (method === 'POST') {
-    const { email, eventId } = req.body;
-    
-}    
-
-const filePath = buildPath();
-  const { events_categories, allEvents } = extractData(filePath);
-
-
-
-
-
+function extractData(filePath) {
+  const jsonData = fs.readFileSync(filePath);
+  const data = JSON.parse(jsonData);
+  return data;
+}
 
 export default function handler(req, res) {
     const { method } = req;
-    if (method === "POST") {
+
+    const filePath = buildPath();
+    const { events_categories, allEvents } = extractData(filePath);
+
+    if (!allEvents) {
+        return res.status(404).json({
+            status: 404,
+            message: 'Events data not found',
+        });
+    }
+    
+    if (method === 'POST') {
         const { email, eventId } = req.body;
-        
-        res.status(200)
-            .json({
-                message: `You has been registered successfully with the email: ${email} ${eventId}`
-            });
+
+        const newAllEvents = allEvents.map((ev) => {
+            if (ev.id === eventId) {
+                if (ev.emails_registered.includes(email)) {
+                    res.status(409).json({ message: 'This email has already been registered' });
+                    return ev;
+                }
+                return {
+                    ...ev,
+                    emails_registered: [...ev.emails_registered, email],
+                };
+            }
+            return ev;
+        });
+        fs.writeFileSync(filePath, JSON.stringify({ events_categories, allEvents: newAllEvents }));
+    
+        res.status(200).json({
+            message: `You have been registered successfully with the email:
+       ${email} for the event: ${eventId}`,
+        });
     }
 }
+
+
+
+
+
+// export default function handler(req, res) {
+//     const { method } = req;
+//     if (method === "POST") {
+//         const { email, eventId } = req.body;
+        
+//         res.status(200)
+//             .json({
+//                 message: `You has been registered successfully with the email: ${email} ${eventId}`
+//             });
+//     }
+// }
