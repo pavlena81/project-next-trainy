74cc5f455acb70696c90661f0ace57f2fafed1b1
Olena Nesteruk <pavlena81@gmail.com>
2023-04-17 23:59:44 +0300

    //

--- pages/api/email-registration.js
+++ pages/api/email-registration.js
@@ -1,3 +1,24 @@
+import path from 'path';
+import fs from 'fs';
+
+function buildPath() {
+  return path.join(process.cwd(), 'data', 'data.json');
+}
+
+const { method } = req;
+if (method === 'POST') {
+    const { email, eventId } = req.body;
+    
+}    
+
+const filePath = buildPath();
+  const { events_categories, allEvents } = extractData(filePath);
+
+
+
+
+
+
 export default function handler(req, res) {
     const { method } = req;
     if (method === "POST") {
